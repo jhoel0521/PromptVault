5f0d86db3e2b03a7c2f480e9711ba127989ac2c9
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2026-01-06 15:11:55 -0400

    feat: Actualizar política de acceso 'PromptPolicy' para permitir acciones de usuario en modelos Prompt

--- app/Policies/PromptPolicy.php
+++ app/Policies/PromptPolicy.php
@@ -13,7 +13,7 @@ class PromptPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class PromptPolicy
      */
     public function view(User $user, Prompt $prompt): bool
     {
-        return false;
+        return $user->id === $prompt->user_id || $prompt->es_publico;
     }
 
     /**
@@ -29,7 +29,7 @@ class PromptPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class PromptPolicy
      */
     public function update(User $user, Prompt $prompt): bool
     {
-        return false;
+        return $user->id === $prompt->user_id;
     }
 
     /**
@@ -45,7 +45,7 @@ class PromptPolicy
      */
     public function delete(User $user, Prompt $prompt): bool
     {
-        return false;
+        return $user->id === $prompt->user_id;
     }
 
     /**
@@ -53,7 +53,7 @@ class PromptPolicy
      */
     public function restore(User $user, Prompt $prompt): bool
     {
-        return false;
+        return $user->id === $prompt->user_id;
     }
 
     /**
@@ -61,6 +61,6 @@ class PromptPolicy
      */
     public function forceDelete(User $user, Prompt $prompt): bool
     {
-        return false;
+        return $user->id === $prompt->user_id;
     }
 }
