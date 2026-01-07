b7104ea2f42712963791f45616e40a09e6fcd23b
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2026-01-07 17:51:47 -0400

    feat: Actualizar tamaño y estilo de los logos en el pie de página y sidebar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -98,7 +98,7 @@
             <!-- Brand -->
             <div class="footer-brand-section">
                 <div class="logo-icon-red">
-                    <img src="{{ asset('images/LogoLoginPrompt.png') }}" alt="PromptVault Logo" class="logo-image" style="width: 70px; height: 70px; object-fit: contain;">
+                    <img src="{{ asset('images/LogoLoginPrompt.png') }}" alt="PromptVault Logo" class="logo-image" style="width: 50px; height: 50px; object-fit: contain;">
                 </div>
                 <div class="brand-text">
                     <span class="brand-title">PROMPTVAULT</span>
